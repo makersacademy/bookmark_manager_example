8369c1f88792b34e6ff9ae45267a833da36e2e55
Sophie Gill <sophie@makersacademy.com>
2018-07-30 16:54:56 +0100

    step 13 - deleting a bookmark

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -7,27 +7,43 @@ class Bookmark
     else
       connection = PG.connect(dbname: 'bookmark_manager')
     end
-    result = connection.exec("SELECT * FROM bookmarks")
-    result.map do |bookmark|
-      Bookmark.new(id: bookmark['id'], title: bookmark['title'], url: bookmark['url'])
+    bookmarks = connection.exec('SELECT * FROM bookmarks;')
+    bookmarks.map do |bookmark|
+      Bookmark.new(
+        id: bookmark['id'],
+        title: bookmark['title'],
+        url: bookmark['url']
+      )
     end
   end
 
   def self.create(url:, title:)
     if ENV['ENVIRONMENT'] == 'test'
-     connection = PG.connect(dbname: 'bookmark_manager_test')
+      connection = PG.connect(dbname: 'bookmark_manager_test')
     else
-     connection = PG.connect(dbname: 'bookmark_manager')
+      connection = PG.connect(dbname: 'bookmark_manager')
     end
-
-    result = connection.exec("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}') RETURNING id, title, url;")
+    result = connection.exec_params(
+      "INSERT INTO bookmarks (url, title) VALUES($1, $2) RETURNING id, title, url;", [url, title]
+    )
     Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
+  end
+
+  def self.delete(id:)
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+    else
+      connection = PG.connect(dbname: 'bookmark_manager')
+    end
+    connection.exec_params(
+      "DELETE FROM bookmarks WHERE id = $1", [id]
+    )
   end
 
   attr_reader :id, :title, :url
 
   def initialize(id:, title:, url:)
-    @id  = id
+    @id = id
     @title = title
     @url = url
   end
