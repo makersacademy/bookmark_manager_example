59c277884e54f520b0513cc3e0fcc2702d1e4ad2
Sophie Gill <sophie@makersacademy.com>
2018-07-30 15:45:27 +0100

    step 7 - interacting with PostgreSQL from Ruby

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@
 
 source "https://rubygems.org"
 
+gem "pg"
 gem "rubocop", require: false
 gem "sinatra"
 
--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,9 +1,9 @@
+require 'pg'
+
 class Bookmark
   def self.all
-    [
-      'http://www.makersacademy.com',
-      'http://www.destroyallsoftware.com',
-      'http://www.google.com',
-    ]
+    connection = PG.connect(dbname: 'bookmark_manager')
+    bookmarks = connection.exec('SELECT * FROM bookmarks;')
+    bookmarks.map { |bookmark| bookmark['url'] }
   end
 end
