4a68cb5d5b31514ec31c7a6672a33ea2db9db55f
Sophie Gill <sophie@makersacademy.com>
2018-08-29 18:26:59 +0100

    step 19 part 1 - authentication happy path

--- app.rb
+++ app.rb
@@ -82,5 +82,15 @@ class BookmarkManager < Sinatra::Base
     redirect '/bookmarks'
   end
 
+  get '/sessions/new' do
+    erb :"sessions/new"
+  end
+
+  post '/sessions' do
+    user = User.authenticate(email: params[:email], password: params[:password])
+    session[:user_id] = user.id
+    redirect('/bookmarks')
+  end
+
   run! if app_file == $0
 end
