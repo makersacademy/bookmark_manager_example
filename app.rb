af48daa0704a955b50429048e8eeb24521660f1b
Sophie Gill <sophie@makersacademy.com>
2018-10-11 13:38:10 +0100

    step 14 - extracting a db setup object

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require_relative './lib/bookmark'
+require_relative './database_connection_setup.rb'
 
 class BookmarkManager < Sinatra::Base
   enable :sessions, :method_override
