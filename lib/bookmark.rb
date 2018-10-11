d2da90db5939848cfe9c898fc7b3813d6748b667
Sophie Gill <sophie@makersacademy.com>
2018-10-11 13:38:10 +0100

    step 15 - extracting a db setup object

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,64 +1,39 @@
-require 'pg'
+require_relative 'database_connection'
 
 class Bookmark
   def self.all
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      connection = PG.connect(dbname: 'bookmark_manager')
-    end
-    bookmarks = connection.exec('SELECT * FROM bookmarks;')
-    bookmarks.map do |bookmark|
+    result = DatabaseConnection.query("SELECT * FROM bookmarks")
+    result.map do |bookmark|
       Bookmark.new(
-        id: bookmark['id'],
+        url: bookmark['url'],
         title: bookmark['title'],
-        url: bookmark['url']
+        id: bookmark['id']
       )
     end
   end
 
   def self.create(url:, title:)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      connection = PG.connect(dbname: 'bookmark_manager')
-    end
-    result = connection.exec_params(
+    result = DatabaseConnection.query(
       "INSERT INTO bookmarks (url, title) VALUES($1, $2) RETURNING id, title, url;", [url, title]
     )
     Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
 
   def self.delete(id:)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      connection = PG.connect(dbname: 'bookmark_manager')
-    end
-    connection.exec_params(
-      "DELETE FROM bookmarks WHERE id = $1", [id]
+    DatabaseConnection.query(
+      "DELETE FROM bookmarks WHERE id = $1", [id] 
     )
   end
 
   def self.update(id:, title:, url:)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      connection = PG.connect(dbname: 'bookmark_manager')
-    end
-    result = connection.exec_params(
+    result = DatabaseConnection.query(
       "UPDATE bookmarks SET url = $1, title = $2 WHERE id = $3 RETURNING id, url, title;", [url, title, id]
     )
     Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
 
   def self.find(id:)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      connection = PG.connect(dbname: 'bookmark_manager')
-    end
-    result = connection.exec_params(
+    result = DatabaseConnection.query(
       "SELECT * FROM bookmarks WHERE id = $1", [id]
     )
     Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
--- spec/database_helpers.rb
+++ spec/database_helpers.rb
@@ -2,5 +2,7 @@ require 'pg'
 
 def persisted_data(id:)
   connection = PG.connect(dbname: 'bookmark_manager_test')
-  connection.query("SELECT * FROM bookmarks WHERE id = '#{id}';")
+  connection.query(
+    "SELECT * FROM bookmarks WHERE id = $1;", [id]
+  )
 end
