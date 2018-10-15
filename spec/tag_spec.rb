f91cd2e337aa33eb459419f2fe655d6e0afff253
Sophie Gill <sophie@makersacademy.com>
2018-10-15 18:19:11 +0100

    step 17 part 2 - filtering bookmarks by tag

--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -1,17 +1,31 @@
 require 'tag'
 require 'bookmark'
+require 'bookmark_tag'
 require 'database_helpers'
 
 describe Tag do
+
+  let(:bookmark_class) { double(:bookmark_class) }
+
   describe '.create' do
-    it 'creates a new Tag object' do
-      tag = Tag.create(content: 'test tag')
+    context 'tag does not exist' do
+      it 'creates a new Tag object' do
+        tag = Tag.create(content: 'test tag')
 
-      persisted_data = persisted_data(id: tag.id, table: 'tags')
+        persisted_data = persisted_data(id: tag.id, table: 'tags')
 
-      expect(tag).to be_a Tag
-      expect(tag.id).to eq persisted_data.first['id']
-      expect(tag.content).to eq 'test tag'
+        expect(tag).to be_a Tag
+        expect(tag.id).to eq persisted_data.first['id']
+        expect(tag.content).to eq 'test tag'
+      end
+    end
+    context 'tag already exists' do
+      it 'returns the existing tag' do
+        tag1 = Tag.create(content: 'test tag')
+        tag2 = Tag.create(content: 'test tag')
+
+        expect(tag2.id).to eq tag1.id
+      end
     end
   end
 
@@ -30,6 +44,27 @@ describe Tag do
       expect(tag).to be_a Tag
       expect(tag.id).to eq tag1.id
       expect(tag.content).to eq tag1.content
+    end
+  end
+
+  describe '.find' do
+    it 'returns a tag with the given id' do
+      tag = Tag.create(content: 'test tag')
+
+      result = Tag.find(id: tag.id)
+
+      expect(result.id).to eq tag.id
+      expect(result.content).to eq tag.content
+    end
+  end
+
+  describe '#bookmarks' do
+    it 'calls .where on the bookmark class' do
+      tag = Tag.create(content: 'test tag')
+
+      expect(bookmark_class).to receive(:where).with(tag_id: tag.id)
+
+      tag.bookmarks(bookmark_class)
     end
   end
 end
